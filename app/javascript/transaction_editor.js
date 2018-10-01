3a32930cbc23f16a45f7859589ec8e8f668bc72e
Shraddha <shraddha.patil@ost.com>
2018-10-01 16:17:01 +0530

    partial actions page

--- app/javascript/transaction_editor.js
+++ app/javascript/transaction_editor.js
@@ -1,18 +1,38 @@
 ;
 (function (window) {
-    function init (config) {
-      var oThis = this;
-      var jUserToUser = $("#kind_user_to_user");
-      console.log("jUserToUser",jUserToUser);
+  function init (config) {
+    var oThis = this;
+    var jUserToUser = $("#kind_user_to_user")
+      , jForm       = $("#transaction_editor_form");
+
+    // var jKind = jForm.find('.j-tx-kind:checked');
+    // tx_kind = jKind.val();
+
+    //console.log("tx_kind",tx_kind);
+    if(jForm.find('#kind_user_to_user:checked') ){
+      console.log("#kind_user_to_user  is true")
     }
-    
-    
+    if(jForm.find('#kind_user_to_comapny:checked') ){
+      console.log("#kind_user_to_company  is true")
+    }
+    if(jForm.find('#kind_company_to_user:checked')){
+      console.log("#kind_company_to_user  is true")
+    }
+    $(".slider-with-input").on('input change',function () {
+      console.log("it changed")
+    })
+
+  }
 
 
 
-  
-    $(document).ready(function () {
-        init();
-    });
+
+
+
+
+
+  $(document).ready(function () {
+    init();
+  });
 
 })(window);
