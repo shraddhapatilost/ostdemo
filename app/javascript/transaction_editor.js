a080aa247ed899e4e472ed6a3832d4e028075424
Shraddha <shraddha.patil@ost.com>
2018-10-03 12:18:13 +0530

    transaction_editor page

--- app/javascript/transaction_editor.js
+++ app/javascript/transaction_editor.js
@@ -9,18 +9,85 @@
     // tx_kind = jKind.val();
 
     //console.log("tx_kind",tx_kind);
-    if(jForm.find('#kind_user_to_user:checked') ){
-      console.log("#kind_user_to_user  is true")
+    // if(jForm.find('#kind_user_to_user:checked') ){
+    //   console.log("#kind_user_to_user  is true")
+    // }
+    // if(jForm.find('#kind_user_to_comapny:checked') ){
+    //   console.log("#kind_user_to_company  is true")
+    // }
+    // if(jForm.find('#kind_company_to_user:checked')){
+    //   console.log("#kind_company_to_user  is true")
+    // }
+
+    jForm.find('.j-tx-kind').change( function () {
+      toggleCommissionsRow();
+
+
+    });
+    jForm.find('.j-action-amount-setting').change(function () {
+      toggleActionAmountRow();
+    });
+    function toggleActionAmountRow(){
+      var jEl       = jForm.find('.j-action-amount-setting:checked')
+        , val       = jEl.val()
+      ;
+      console.log("val",val);
+      if(val === "true"){
+        jForm.find("#j-action-amount-wrap").slideUp(300);
+      }
+      else{
+        jForm.find("#j-action-amount-wrap").slideDown(300);
+
+      }
     }
-    if(jForm.find('#kind_user_to_comapny:checked') ){
-      console.log("#kind_user_to_company  is true")
+
+     function toggleCommissionsRow () {
+
+
+      var jEl       = jForm.find('.j-tx-kind:checked')
+        , val       = jEl.val()
+      ;
+
+      if ( "user_to_user" === val ) {
+        jForm.find("#commission-options-row").slideDown( 300 );
+      } else {
+        jForm.find("#commission-options-row").slideUp( 300 );
+      }
+
     }
-    if(jForm.find('#kind_company_to_user:checked')){
-      console.log("#kind_company_to_user  is true")
-    }
-    $(".slider-with-input").on('input change',function () {
-      console.log("it changed")
-    })
+    // $(".slider-with-input").on('input change',function () {
+    //   console.log($('#commission_percent_slider').val());
+    //   var sliderResult =  $('#commission_percent_slider').val()
+    //   $("#slider-result").val(sliderResult);
+    // })
+
+    $('#commission_percent_slider').slider({
+      formatter: function(value) {
+        return 'Current value: ' + value;
+      }
+    });
+
+    var jCommission             = $("#commission_percent");
+    var jValueInFiat            = $("#value_in_fiat");
+    console.log("jCommission",jCommission.length);
+    var vCommission = jCommission.val()
+      , vFiat       = jValueInFiat.val()
+    console.log("vCommission",vCommission);
+
+    // var jValueInOst             = $("#value_in_ost");
+    // cInFiat     = vFiat.times( vCommission ).div( 100 );
+    //
+    // oThis.jCInBt.setVal( toBt( cInBt ) );
+    //
+    //  function toBt ( bt ) {
+    //   var oThis = this;
+    //
+    //   if ( oThis.isNaN( bt ) ) {
+    //     return NaN;
+    //   }
+    //   bt = BigNumber( bt );
+    //   return BigNumber( bt.toFixed(P_BT, P_BT_ROUND_ROUNDING_MODE) );
+    // }
 
   }
 
